01658f95655170aff34820d09dd3bdae47c76fcc
Linh Pham <phamlinh@grinnell.edu>
2017-09-12 15:28:15 +0000

    Finish part 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,37 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    #Create all ratings variables
+    @all_ratings =  Movie.all_ratings
+    #Set default values for parameters
+    @defaults = {ratings: nil, sort_by: nil}
+    params.replace(@defaults.merge(params)) 
+    #Variables for storing the selected ratings
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
+    if @selected_ratings == {}
+      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+      session[:ratings] = @selected_ratings
+    end
+    #Filter movies based on selected ratings
+    @movies = Movie.where(rating: @selected_ratings.keys).order(params[:sort_by])
+    @sort_column = params[:sort_by]
+    #Store parameters into session
+    if (params[:sort_by].to_s == 'title')
+      session[:sort_by] = params[:sort_by]
+    elsif(params[:sort_by].to_s == 'release_date')
+      session[:sort_by] = params[:sort_by]
+    else session[:sort_by] = nil
+    # elsif(params[:sort_by] != nil)
+    #   session[:sort_by] = params[:sort_by]
+    # elsif(session.has_key?(:sort_by))
+    #   params[:sort_by] = session[:sort_by]
+    end
+  
+    if(params[:ratings] != nil)
+      session[:ratings] = params[:ratings]
+    elsif(session.has_key?(:ratings))
+      params[:ratings] = session[:ratings]
+    end
   end
 
   def new
